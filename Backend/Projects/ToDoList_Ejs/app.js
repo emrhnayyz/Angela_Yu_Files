448d7e0f01da34efb1c34e35ab0a2942fb2eb66d
emrhnayz <emirhan.ayaz171@gmail.com>
2023-09-17 01:26:46 +0200

    v2

--- Backend/Projects/ToDoList_Ejs/app.js
+++ Backend/Projects/ToDoList_Ejs/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 const app = express();
+app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.get("/", (req,res)=>{
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+    let today= new Date()
+    let day = today.toLocaleDateString("en-Us",options)
+    res.render("list", {KingOfDay: day})
+})
+
+
+
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("port is running on port 3000");
 })
