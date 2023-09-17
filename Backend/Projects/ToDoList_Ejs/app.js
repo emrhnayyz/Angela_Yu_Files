27ab42eabcec142a3d4db582572f2a2f27e206bc
emrhnayz <emirhan.ayaz171@gmail.com>
2023-09-17 22:48:48 +0200

    without_css_lastv

--- Backend/Projects/ToDoList_Ejs/app.js
+++ Backend/Projects/ToDoList_Ejs/app.js
@@ -6,14 +6,22 @@ const app = express();
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
+let newItems = []
+
 app.get("/", (req,res)=>{
     let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
     let today= new Date()
     let day = today.toLocaleDateString("en-Us",options)
-    res.render("list", {KingOfDay: day})
+    res.render("list", {kindOfDay: day,newListItems: newItems})
 })
 
+app.post('/', (req,res)=>{
+    let newItem = req.body.newItem;
+    newItems.push(newItem)
+    res.redirect('/')
+})
 
 
 app.listen(3000, () => {
