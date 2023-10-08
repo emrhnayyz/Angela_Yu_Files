6b529d234486c702c59c8a39a91519ca16edc939
emrhnayz <emirhan.ayaz171@gmail.com>
2023-10-08 08:40:18 +0200

    last_version2

--- Backend/5.6 Secrets Project/index.js
+++ Backend/5.6 Secrets Project/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-app.use(express.static("puclic"));
+app.use(express.static("public"));
 
 app.get("/", async(req,res)=>{
     try {
