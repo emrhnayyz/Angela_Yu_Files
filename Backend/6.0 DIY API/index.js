2df6c63a15926ef9812a477f2102c5aa6474ebd0
emrhnayz <emirhan.ayaz171@gmail.com>
2023-10-12 23:26:54 +0200

    v4

--- Backend/6.0 DIY API/index.js
+++ Backend/6.0 DIY API/index.js
@@ -15,14 +15,38 @@ app.get("/random", (req,res)=>{
 })
 
 //2. GET a specific joke
-
+app.get("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const foundJoke = jokes.find((joke) => joke.id === id);
+  res.json(foundJoke);
+});
 
 
 
 
 //3. GET a jokes by filtering on the joke type
+app.get("/filter", (req, res) => {
+  const type = req.query.type;
+  const filteredActivities = jokes.filter((joke) => joke.jokeType === type);
+  res.json(filteredActivities);
+});
+
+
+
+
 
 //4. POST a new joke
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.json(newJoke);
+});
+
 
 //5. PUT a joke
 
